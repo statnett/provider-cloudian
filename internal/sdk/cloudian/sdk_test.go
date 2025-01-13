2c7cd3c1d80c4648e11f811cf41981ea0958b25d
Sjur Millidahl <sjur.millidahl@statnett.no>
2025-01-13 17:19:35 +0100

    test: mock pagination in order to test sdk cloudian ListUsers

--- internal/sdk/cloudian/sdk_test.go
+++ internal/sdk/cloudian/sdk_test.go
@@ -29,25 +29,22 @@ func TestWrappedErrNotFound(t *testing.T) {
 }
 
 func TestGetGroup(t *testing.T) {
-	cloudianClient, testServer := mockBy(func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(groupInternal{GroupID: "QA", Active: "true"})
-	})
-	defer testServer.Close()
-
 	expected := Group{
 		GroupID: "QA",
 		Active:  true,
 	}
+	cloudianClient, testServer := mockBy(func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(toInternal(expected))
+	})
+	defer testServer.Close()
 
 	group, err := cloudianClient.GetGroup(context.TODO(), "QA")
 	if err != nil {
 		t.Errorf("Error getting group: %v", err)
 	}
-
 	if diff := cmp.Diff(expected, *group); diff != "" {
 		t.Errorf("GetGroup() mismatch (-want +got):\n%s", diff)
 	}
-
 }
 
 func TestGetGroupNotFound(t *testing.T) {
@@ -74,7 +71,6 @@ func TestCreateCredentials(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error creating credentials: %v", err)
 	}
-
 	if diff := cmp.Diff(expected, *credentials); diff != "" {
 		t.Errorf("CreateUserCredentials() mismatch (-want +got):\n%s", diff)
 	}
@@ -88,11 +84,9 @@ func TestGetUserCredentials(t *testing.T) {
 	defer testServer.Close()
 
 	credentials, err := cloudianClient.GetUserCredentials(context.TODO(), "123")
-
 	if err != nil {
 		t.Errorf("Error getting credentials: %v", err)
 	}
-
 	if diff := cmp.Diff(expected, *credentials); diff != "" {
 		t.Errorf("GetUserCredentials() mismatch (-want +got):\n%s", diff)
 	}
@@ -111,36 +105,48 @@ func TestListUserCredentials(t *testing.T) {
 	credentials, err := cloudianClient.ListUserCredentials(
 		context.TODO(), User{UserID: "", GroupID: ""},
 	)
-
 	if err != nil {
 		t.Errorf("Error listing credentials: %v", err)
 	}
-
 	if diff := cmp.Diff(expected, credentials); diff != "" {
 		t.Errorf("ListUserCredentials() mismatch (-want +got):\n%s", diff)
 	}
 }
 
 func TestListUsers(t *testing.T) {
-	expected := []User{{GroupID: "QA", UserID: "user1"}}
+	mkUsers := func(offset, n int) []User {
+		users := make([]User, 0)
+		for i := offset; i < n; i++ {
+			users = append(users, User{GroupID: "QA", UserID: fmt.Sprintf("user%d", i)})
+		}
+		return users
+	}
+	// We pretend 102 users exist in the cloudian server
+	expected := mkUsers(0, 102)
+
 	cloudianClient, testServer := mockBy(func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(expected)
+		if r.URL.Query().Get("offset") == "" {
+			// return 101 users in the first batch (the 101th indicating there are "more results")
+			json.NewEncoder(w).Encode(expected[:101])
+		} else {
+			// return the two last users (0-indexed) as the last batch
+			json.NewEncoder(w).Encode(expected[100:])
+		}
 	})
 	defer testServer.Close()
 
-	users, err := cloudianClient.ListUsers(context.TODO(), "QA", nil)
-
+	// the first 101 users (indicating "more results" from server)
+	users, err := cloudianClient.ListUsers(context.Background(), "QA", nil)
 	if err != nil {
 		t.Errorf("Error listing users: %v", err)
 	}
-
 	if diff := cmp.Diff(expected, users); diff != "" {
-		t.Errorf("ListUsers() mismatch (-want +got):\n%s", diff)
+		t.Errorf("ListUsers() mismatch without offset (-want +got):\n%s", diff)
 	}
+
 }
 
 func mockBy(handler http.HandlerFunc) (*Client, *httptest.Server) {
 	mockServer := httptest.NewServer(handler)
-
 	return NewClient(mockServer.URL, ""), mockServer
 }
